e9dead403cae4d004c59db43df84d5bb4cc4816f
Adam Kecskes <adam@kecskes.net>
2020-04-13 13:52:54 -0500

    Added Feature section

--- src/components/App.js
+++ src/components/App.js
@@ -12,12 +12,31 @@ import Footer from './Footer';
 import Aside from './Aside';
 import ContentCard from './ContentCard';
 import Showcase from './Showcase';
+import Feature from './Feature';
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const sections = [
   { title: 'Blog', url: '#' },
   { title: 'Projects', url: '#' },
   { title: 'About', url: '#' },
+];
+const featuredPosts = [
+  {
+    title: 'Featured post',
+    date: 'Nov 12',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
 ];
 
 export default function App() {
@@ -30,6 +49,11 @@ export default function App() {
       <Navigation title='Adam Kecskes Consulting' sections={sections} />
       <main>
         <Showcase />
+        <Grid container spacing={4}>
+          {featuredPosts.map((post) => (
+            <Feature key={post.title} post={post} />
+          ))}
+        </Grid>
         <Container className={classes.cardGrid} maxWidth='md'>
           <Grid container spacing={4}>
             {cards.map((card) => (
