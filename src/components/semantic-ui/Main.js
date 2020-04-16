41432fdbf1e352834d8d1219122ea32410a954e1
Adam K <adam@kecskes.net>
2020-04-15 21:51:48 -0500

    indent the domain list and turn it into array to make the code more readable

--- src/components/semantic-ui/Main.js
+++ src/components/semantic-ui/Main.js
@@ -6,6 +6,17 @@ import { List, Icon, Segment, Container, Grid, Image } from 'semantic-ui-react';
 import SegmentDivider from './SegmentDivider';
 import CircularSegment from './CircularSegment';
 
+const domains = [
+  'Server & Client Software Development',
+  'UX/UI Design Implementation',
+  'Product Lifecycle Management',
+  'Optimizing Operations',
+  'Program Management',
+  'Process Improvement',
+  'Technical Staffing',
+  'Quality Assurance',
+];
+
 const Main = (props) => {
   return (
     <Segment basic style={{ padding: '3.5em 0em' }} vertical>
@@ -18,11 +29,21 @@ const Main = (props) => {
               </CircularSegment>
             </Grid.Column>
             <Grid.Column width={10}>
-              <SimpleCard
-                className='top'
-                title='What I Offer to Your Company'
-                body="I'm a natural problem solver; instead of putting up with the status quo, I'm always asking 'What improvements can I make? As such, I've moved up to Director and VP positions at various companies because that is were I can provide the greatest level of service. However, I'm not afraid to dig into code, databases, or documentation when necessary. I see it as essential for truly understanding what my team is going through."
-              ></SimpleCard>
+              <SimpleCard className='top' title='What I Offer to Your Company'>
+                <p>
+                  I'm a natural problem solver; instead of putting up with the
+                  status quo, I'm always asking, 'What improvements can we make
+                  to make our efforts a little easier?
+                  <br />
+                  <br />
+                  As such, I've moved up to Director and VP positions at various
+                  companies because that is were I can provide the greatest
+                  level of service. However, I'm not afraid to dig into code,
+                  databases, or documentation when necessary. Often times the
+                  details reveal issues &mdash; and solutions &mdash; which will
+                  impact the entire organizations.
+                </p>
+              </SimpleCard>
             </Grid.Column>
           </Grid.Row>
           <Grid.Row centered>
@@ -34,46 +55,26 @@ const Main = (props) => {
           </Grid.Row>
           <Grid.Row centered>
             <Grid.Column width={10} vertical>
-              <SimpleCard
-                title='I Wear Multiple Hats'
-                body="I've worked for 3 large cap companies and 3 startup companies. I've had to wear a lot of hats and put my finger in a lot of cookie jars."
-              ></SimpleCard>
-              <List relaxed size='huge'>
-                <List.Item>
-                  {' '}
-                  <Icon name='chevron right' />
-                  Server &amp; Client Software Development
-                </List.Item>
-                <List.Item>
-                  {' '}
-                  <Icon name='chevron right' />
-                  UX/UI Design Implementation
-                </List.Item>
-                <List.Item>
-                  {' '}
-                  <Icon name='chevron right' />
-                  Product Lifecycle Management{' '}
-                </List.Item>
-                <List.Item>
-                  {' '}
-                  <Icon name='chevron right' />
-                  Optimizing Operations
-                </List.Item>
-                <List.Item>
-                  {' '}
-                  <Icon name='chevron right' />
-                  Program Management
-                </List.Item>
-                <List.Item>
-                  {' '}
-                  <Icon name='chevron right' />
-                  Process Improvement
-                </List.Item>
-                <List.Item>
-                  {' '}
-                  <Icon name='chevron right' />
-                  Technical Staffing
-                </List.Item>
+              <SimpleCard title='I Wear Multiple Hats' body=''>
+                <p>
+                  I've worked for 3 large cap companies and 3 startup companies
+                  <br />
+                  all at various stages of maturity,&nbsp;&nbsp;giving me a wide
+                  range of
+                  <br />
+                  business experiences. Some of the domains I've worked in:
+                </p>
+              </SimpleCard>
+              <List relaxed size='huge' style={{ marginLeft: '50px' }}>
+                {domains.map((domain) => {
+                  return (
+                    <List.Item>
+                      {' '}
+                      <Icon name='chevron right' />
+                      {domain}
+                    </List.Item>
+                  );
+                })}
               </List>
             </Grid.Column>
             <Grid.Column width={2}></Grid.Column>
