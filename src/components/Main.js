1e35104e61831e2bc8bb00c096e4680384073d05
Adam K <adam@kecskes.net>
2020-05-20 09:22:35 -0500

    Text updates, color change, and spell checks

--- src/components/Main.js
+++ src/components/Main.js
@@ -32,17 +32,25 @@ const Main = (props) => {
             <Grid.Column width={10}>
               <SimpleCard className='top' title='I Ask a Lot of Questions'>
                 <p>
-                  I'm a troubleshooter; instead of putting up with the status
-                  quo, I'm always asking, 'What improvements can we make to make
-                  our efforts a little easier?
-                  <br />
-                  <br />
-                  As such, I've moved up to Director and VP positions at various
-                  companies because that is were I can provide the greatest
-                  level of service. However, I'm not afraid to dig into code,
-                  databases, or documentation when necessary. Often times it's
-                  looking into the details that reveal issues &mdash; and
-                  solutions &mdash; which will impact the entire organization.
+                  <p>
+                    I'm a troubleshooter; instead of putting up with the status
+                    quo, I'm always asking, 'What improvements can we make to
+                    make our efforts a little easier?
+                  </p>
+                  <p>
+                    I also maintain a customer (and people in general!) centeric
+                    worldview. The question I ask myself is, "Which solutions
+                    benefit the most of us, and still respect all of us? "
+                  </p>
+                  <p>
+                    As such, I've moved up to Director and VP positions at
+                    various companies because that is were I can provide the
+                    greatest level of service. However, I'm not afraid to dig
+                    into code, databases, or documentation when necessary. Often
+                    times it's looking into the details that reveal issues
+                    &mdash; and solutions &mdash; which will best impact the
+                    entire organization.
+                  </p>
                 </p>
               </SimpleCard>
             </Grid.Column>
--- src/content/Portfolio.md
+++ src/content/Portfolio.md
@@ -1,4 +1,4 @@
-# Current Coding Portoflio
+# Current Coding Portfolio
 
 This is a work in progress. You can find the work I'm doing at GitHub: [`https://github.com/adamk72`](#https://github.com/adamk72)
 
@@ -9,9 +9,9 @@ This is a work in progress. You can find the work I'm doing at GitHub: [`https:/
 - **Professional Progress Website**
   - The site you're looking it, done in ReactJS.
 - **Personal Activity Tracker for FitBit**
-  - A simple app where a user inputs a list of activites to be tracked; at the start of each activity, the user can quickly start the timer. The goal is for the user to be more conscious of where time in their day is spent.
+  - A simple app where a user inputs a list of activities to be tracked; at the start of each activity, the user can quickly start the timer. The goal is for the user to be more conscious of where time in their day is spent.
 - **State of Mind Tracker for FitBit**
-  - Similar to the tracker, but the user tracks their choices and action from a more philosophical point of view, following the precepts of _Monk, Marshallow, or Monster_.
+  - Similar to the tracker, but the user tracks their choices and action from a more philosophical point of view, following the precepts of _Monk, Marshmallow, or Monster_.
   -
 - **Esoteric Word Website**
-  - As a bit of word nerd and public speaking trainer, I want to promote words that are not as well used, but I think would really add flavor to our conversations and definitely take up a professional speakera anoter notch.
+  - As a bit of word nerd and public speaking trainer, I want to promote words that are not as well-used, but I think would really add flavor to our conversations and definitely take up a professional speaker another notch.
