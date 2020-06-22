7b5898b3711343fd13d5196f44b212ad38be1780
Adam K <adam@kecskes.net>
2020-06-22 18:35:53 -0500

    Got icons to show up on social list

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,29 +1,34 @@
 import { GET_SOCIAL_INFO } from './actionTypes';
+import React from 'react';
+import { FaAt, FaGithub, FaLinkedin, FaPhone } from 'react-icons/fa';
 
 export function getSocialInfo() {
   // this is static date; in the future I may bind it to something more dynamic.
-  const payload = [
-    {
-      href:
-        'mailto://adam@kecskes.net?subject=Business%20Inquiry%20from%20Website',
-      icon: null,
-      text: 'Foo-Email',
-    },
-    {
-      href: 'https://github.com/adamk72',
-      icon: null,
-      text: 'GitHub',
-    },
-    {
-      href: 'https://linkedin.com/in/adamkecskes/',
-      icon: null,
-      text: 'LinkedIn',
-    },
-    {
-      href: 'tel://512-662-2969',
-      icon: null,
-      text: '512-662-2969',
-    },
-  ];
+  const payload = {
+    IconList: [<FaAt />, <FaGithub />, <FaLinkedin />, <FaPhone />],
+    ContentList: [
+      {
+        href:
+          'mailto://adam@kecskes.net?subject=Business%20Inquiry%20from%20Website',
+        icon: 'FaAt',
+        text: 'Email',
+      },
+      {
+        href: 'https://github.com/adamk72',
+        icon: null,
+        text: 'GitHub',
+      },
+      {
+        href: 'https://linkedin.com/in/adamkecskes/',
+        icon: null,
+        text: 'LinkedIn',
+      },
+      {
+        href: 'tel://512-662-2969',
+        icon: null,
+        text: '512-662-2969',
+      },
+    ],
+  };
   return { type: GET_SOCIAL_INFO, payload };
 }
--- src/content/SocialList.js
+++ src/content/SocialList.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
-import { FaAt, FaGithub, FaPhone, FaLinkedin } from 'react-icons/fa';
 import { getSocialInfo } from '../actions';
 
 const SocialList = ({ socialInfo }) => {
   return (
     <React.Fragment>
       <ListGroup>
-        {socialInfo.payload.map((item, index) => (
+        {socialInfo.payload.ContentList.map((item, index) => (
           <ListGroup.Item key={index}>
-            <Button href={item.href}>{item.text}</Button>
+            <Button href={item.href}>
+              {socialInfo.payload.IconList[index]} {item.text}
+            </Button>
           </ListGroup.Item>
         ))}
       </ListGroup>
