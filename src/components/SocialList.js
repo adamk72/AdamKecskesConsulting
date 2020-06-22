38815e75adc8954423a4dee30065cf622d61b023
Adam K <adam@kecskes.net>
2020-06-22 18:38:59 -0500

    Moved SocialList to Components folder

---
+++ src/components/SocialList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import { getSocialInfo } from '../actions';
+
+const SocialList = ({ socialInfo }) => {
+  return (
+    <React.Fragment>
+      <ListGroup>
+        {socialInfo.payload.ContentList.map((item, index) => (
+          <ListGroup.Item key={index}>
+            <Button href={item.href}>
+              {socialInfo.payload.IconList[index]} {item.text}
+            </Button>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </React.Fragment>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    socialInfo: getSocialInfo(),
+  };
+};
+
+export default connect(mapStateToProps)(SocialList);
