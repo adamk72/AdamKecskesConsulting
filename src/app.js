4f5bafceafd090879a8f73bd171f733b2f1a3d25
Adam K <adam@kecskes.net>
2020-06-21 21:02:43 -0500

    restarting with Bootstrap; had to rearrange files to support scss

---
+++ src/app.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Homepage from './content/Homepage';
+import BasicMdPage from './components/BasicMdPage';
+import workPageContent from '../src/content/md_files/WorkExperience.md';
+import thespreadsheet from '../src/content/md_files/TheSpreadsheet.md';
+import automatingreporting from '../src/content/md_files/AutomatingTheReporting.md';
+import theproductlifecycle from '../src/content/md_files/TheProductLifeCycle.md';
+import portfolioPageContent from '../src/content/md_files/Portfolio.md';
+import philosophyPageContent from '../src/content/md_files/Philosophy.md';
+
+export function App() {
+  return (
+    <main>
+      <Switch>
+        <Route path='/' exact component={Homepage} />
+        <Route
+          path='/portfolio'
+          component={() => <BasicMdPage mdContent={portfolioPageContent} />}
+        />
+        <Route
+          path='/work'
+          component={() => <BasicMdPage mdContent={workPageContent} />}
+        />
+        <Route
+          path='/optimization'
+          component={() => <BasicMdPage mdContent={philosophyPageContent} />}
+        />
+        <Route
+          path='/thespreadsheet'
+          component={() => <BasicMdPage mdContent={thespreadsheet} />}
+        />
+        <Route
+          path='/automatingreporting'
+          component={() => <BasicMdPage mdContent={automatingreporting} />}
+        />
+        <Route
+          path='/theproductlifecycle'
+          component={() => <BasicMdPage mdContent={theproductlifecycle} />}
+        />
+      </Switch>
+    </main>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
-import Homepage from './content/Homepage';
-import BasicMdPage from './components/BasicMdPage';
-import workPageContent from '../src/content/md_files/WorkExperience.md';
-import thespreadsheet from '../src/content/md_files/TheSpreadsheet.md';
-import automatingreporting from '../src/content/md_files/AutomatingTheReporting.md';
-import theproductlifecycle from '../src/content/md_files/TheProductLifeCycle.md';
-import portfolioPageContent from '../src/content/md_files/Portfolio.md';
-import philosophyPageContent from '../src/content/md_files/Philosophy.md';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app';
+import './scss/index.scss';
 
 ReactDOM.render(
-  <Router basename='/'>
-    <Route path='/' exact component={Homepage} />
-    <Route
-      path='/portfolio'
-      component={() => <BasicMdPage mdContent={portfolioPageContent} />}
-    />
-    <Route
-      path='/work'
-      component={() => <BasicMdPage mdContent={workPageContent} />}
-    />
-    <Route
-      path='/optimization'
-      component={() => <BasicMdPage mdContent={philosophyPageContent} />}
-    />
-    <Route
-      path='/thespreadsheet'
-      component={() => <BasicMdPage mdContent={thespreadsheet} />}
-    />
-    <Route
-      path='/automatingreporting'
-      component={() => <BasicMdPage mdContent={automatingreporting} />}
-    />
-    <Route
-      path='/theproductlifecycle'
-      component={() => <BasicMdPage mdContent={theproductlifecycle} />}
-    />
-  </Router>,
-
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
