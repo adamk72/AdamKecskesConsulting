e6ae974c9abe70ad3674f7d395b01f72748e5c92
Adam K <adam@kecskes.net>
2020-06-23 11:10:53 -0500

    Fix for warning of link requiring a 'to' element

--- src/content/MainMenuFragment.js
+++ src/content/MainMenuFragment.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Dropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
@@ -7,31 +6,25 @@ export default function MainDropdownFragment({ isMobile }) {
   return (
     <Dropdown>
       <LinkContainer to='/'>
-        <Dropdown.Item as={Link} className='mainDropdownItem'>
-          Home
-        </Dropdown.Item>
+        <Dropdown.Item className='mainDropdownItem'>Home</Dropdown.Item>
       </LinkContainer>
       <LinkContainer to='/optimization'>
-        <Dropdown.Item as={Link} className='mainDropdownItem'>
-          Optimization
-        </Dropdown.Item>
+        <Dropdown.Item className='mainDropdownItem'>Optimization</Dropdown.Item>
       </LinkContainer>
       <LinkContainer to='/work'>
-        <Dropdown.Item as={Link} className='mainDropdownItem'>
-          Abridged CV
-        </Dropdown.Item>
+        <Dropdown.Item className='mainDropdownItem'>Abridged CV</Dropdown.Item>
       </LinkContainer>
       {/* <Dropdown className='link item mainDropdownItem' text='Case Studies'>
         <Dropdown.Dropdown>
-          <Dropdown.Item as={Link} to='/thespreadsheet'>
+          <Dropdown.Item to='/thespreadsheet'>
             The Spreadsheet
           </Dropdown.Item>
 
-          <Dropdown.Item as={Link} to='/automatingreporting'>
+          <Dropdown.Item  to='/automatingreporting'>
             Automating the Reporting
           </Dropdown.Item>
 
-          <Dropdown.Item as={Link} to='/theproductlifecycle'>
+          <Dropdown.Item  to='/theproductlifecycle'>
             The Product Lifecycle
           </Dropdown.Item>
         </Dropdown.Dropdown>
