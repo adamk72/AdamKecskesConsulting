8de57f5f46137a38d794ca3266232e9589860adc
Adam K <adam@kecskes.net>
2020-06-24 17:30:08 -0500

    Got lines on section divider to display properly

--- src/components/SectionDivider.js
+++ src/components/SectionDivider.js
@@ -2,7 +2,11 @@ import React from 'react';
 // const INIT_CHILD = String.fromCharCode(8943); // centered horizontal ellipsis
 
 const SectionDivider = (props) => {
-  return <h6 className='sectionDivider horizontal'>{props.content}</h6>;
+  return (
+    <div>
+      <h6 className='sectionDivider horizontal'>{props.content}</h6>
+    </div>
+  );
 };
 
 export default SectionDivider;
