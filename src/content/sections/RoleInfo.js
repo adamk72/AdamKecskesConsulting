ef86f2a86e427571d33eac7f97be654a836d6301
Adam K <adam@kecskes.net>
2020-06-25 12:37:54 -0500

    Put an accordion on and bifurcate the domains list

--- src/content/sections/RoleInfo.js
+++ src/content/sections/RoleInfo.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import SimpleCard from '../../components/SimpleCard';
-import { ListGroup } from 'react-bootstrap';
+import { ListGroup, Card, Accordion, Button } from 'react-bootstrap';
 import CircularEnclose from '../../components/CircularEnclose';
 import { FaChevronRight } from 'react-icons/fa';
-const domains = [
+const techDomains = [
   { key: 1, text: 'Server & Client Agile Software Development' },
   { key: 3, text: 'UX/UI Design Implementation' },
   { key: 2, text: 'Product Lifecycle Origination' },
-  { key: 10, text: 'Knowledge Management' },
   { key: 9, text: 'Information Archiecture' },
+  { key: 7, text: 'Quality Assurance' },
+];
+
+const pmDomains = [
+  { key: 8, text: 'Technical Staffing' },
   { key: 4, text: 'Optimizing Operations' },
   { key: 5, text: 'Program Management' },
   { key: 6, text: 'Process Improvement' },
-  { key: 7, text: 'Quality Assurance' },
-  { key: 8, text: 'Technical Staffing' },
+  { key: 10, text: 'Knowledge Management' },
 ];
 
 export const RoleRightSide = [
@@ -26,7 +29,7 @@ export const RoleRightSide = [
       </p>
       <p>Some of the domains I've worked in:</p>
     </SimpleCard>
-    <ListGroup>
+    {/* <ListGroup>
       {domains.map((domain) => {
         return (
           <ListGroup.Item key={domain.key}>
@@ -35,7 +38,41 @@ export const RoleRightSide = [
           </ListGroup.Item>
         );
       })}
-    </ListGroup>
+    </ListGroup> */}
+    <Accordion defaultActiveKey='0'>
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant='link' eventKey='1'>
+            Technology
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey='1'>
+          <Card.Body>
+            <ul>
+              {techDomains.map((domain) => {
+                return <li>{domain.text}</li>;
+              })}
+            </ul>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant='link' eventKey='2'>
+            Program Management
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey='2'>
+          <Card.Body>
+            <ul>
+              {pmDomains.map((domain) => {
+                return <li>{domain.text}</li>;
+              })}
+            </ul>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
     ,
   </>,
 ];
