766721422011aa9383748e91b2c07832fc3239a1
Adam K <adam@kecskes.net>
2020-06-25 12:55:26 -0500

    Indent the proclamations in the into section

--- src/content/sections/IntroInfo.js
+++ src/content/sections/IntroInfo.js
@@ -21,10 +21,12 @@ export const CardText = [
       className='top justify'
       body="Hi! I'm Adam Kecskes, and I've done a lot to help companies thrive over the 20 years of my career in technology. Here's how I consider myself; I'm a:"
     />
-    <SimpleCard className='announce' title='Operational Optimizer' />
-    <SimpleCard className='announce' title='Program Orchestrator' />
+    <div id='heroProclamation'>
+      <SimpleCard className='announce' title='Operational Optimizer' />
+      <SimpleCard className='announce' title='Program Orchestrator' />
 
-    <SimpleCard className='announce' title='Customer Advocate' />
+      <SimpleCard className='announce' title='Customer Advocate' />
+    </div>
     <SimpleCard body="... and so much more. Most importantly, I'm here to help your company overcome a range of problems." />
   </>,
 ];
