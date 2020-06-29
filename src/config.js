8f490c7e35a8193a3c7b8f22ea00614f2d90ece1
Adam K <adam@kecskes.net>
2020-06-29 09:23:15 -0500

    replace config with proper path

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default function getRemotePath() {
   switch (process.env.NODE_ENV) {
     case 'production':
-      return '/k_test_site';
+      return '/';
     case 'development':
     case 'test':
     default:
