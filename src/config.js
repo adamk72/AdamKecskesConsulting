87214b65bca7dcf8df84caaf033d1c517a5af913
Adam K <adam@kecskes.net>
2020-06-29 09:37:24 -0500

    Reset config.js so that can now develope on the root of localhost

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default function getRemotePath() {
   switch (process.env.NODE_ENV) {
+    case 'development':
     case 'production':
       return '/';
-    case 'development':
     case 'test':
     default:
       return '/k_test_site';
