7aa3a709d3bb300a99d8e1d102e98077a6c27f59
Adam K <adam@kecskes.net>
2020-06-29 09:20:56 -0500

    Get LiteTitle to align properly using display:flex

--- src/content/LiteTitle.js
+++ src/content/LiteTitle.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Row, Container, Col } from 'react-bootstrap';
 import SocialList from '../components/SocialList';
 
 const LiteTitle = ({ mobile }) => (
-  <Container className='liteTitle'>
-    <Row>
-      <Col className='header title top-item col-sm'>
-        <h1>Adam Kecskes</h1>
-      </Col>
-      <Col className='header bottom-item col-sm'>
-        <SocialList />
-      </Col>
-    </Row>
-  </Container>
+  <div className='liteTitle'>
+    <h1>Adam Kecskes</h1>
+    <SocialList />
+  </div>
 );
 
 LiteTitle.propTypes = {
