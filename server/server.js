94694c7d5ed9b80af3a90f97bd214eb70159c89b
Adam K <adam@kecskes.net>
2020-07-23 15:06:05 -0500

    return test JSON data

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ require('dotenv').config()
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const nodemailerRouter = require('./routes/nodemailer');
+const testRouter = require('./routes/test');
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/nodemailer', nodemailerRouter);
+app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- src/content/sections/GetSpecialOffer.js
+++ src/content/sections/GetSpecialOffer.js
@@ -5,15 +5,17 @@ import { Button } from 'react-bootstrap'
 const GetSpecialOffer = () => {
     const [input, setInput] = useState('')
 
+    const handleSubmit = (e) => {
 
+    }
 
     return (
 
         <div className="special-offer">
             <span className="call-to-action">Get my whitepaper on which questions non-coders should be asking their software devs:</span>
             <form>
-                <input name="emailInput" placeholder="Enter email" onChange={(e) => { setInput(e.target.value); console.log(input) }} />&nbsp;&nbsp;&nbsp;
-                <Button variant="success">Submit</Button>
+                <input name="emailInput" placeholder="Enter email" onChange={(e) => setInput(e.target.value)} />&nbsp;&nbsp;&nbsp;
+                <Button variant="success" onClick={() => handleSubmit} > Submit</Button>
             </form>
 
         </div>
