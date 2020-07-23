dacf7da8085e50b1bfa54582f3a134cbd7aa8d32
Adam K <adam@kecskes.net>
2020-07-23 11:33:05 -0500

    got nodemailer basic working

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
+require('dotenv').config()
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const nodemailerRouter = require('./routes/nodemailer');
 
 const app = express();
 
@@ -26,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/nodemailer', nodemailerRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
